c2c8a33f9646847ebd44dd57f8569c5223127180
vicbro <vicbro@hotmail.no>
2025-05-01 23:15:54 +0200

    Fixing fetching posts issue

--- src/js/api/post/display.js
+++ src/js/api/post/display.js
@@ -1,4 +1,4 @@
-import { JWT_TOKEN } from "/fed2-js2-ca-vicbro00/src/js/api/constants.js";
+import { JWT_TOKEN, API_BASE_URL } from "/fed2-js2-ca-vicbro00/src/js/api/constants.js";
 
 export async function fetchPostsWithAuthors() {
     const token = localStorage.getItem(JWT_TOKEN);
